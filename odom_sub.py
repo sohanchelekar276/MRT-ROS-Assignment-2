6e501e54e35a9f9863557b36f51f355ed1c65842
sohanchelekar276 <sohan.chelekar@gmail.com>
2025-11-29 21:20:36 +0530

    Update odom_sub.py

--- odom_sub.py
+++ odom_sub.py
@@ -1,40 +1,36 @@
 import rclpy
 from rclpy.node import Node
-from rover_odometry.msg import RoverOdometry
+from mars_msgs.msg import RoverOdometry
 import math
 
-class OdometrySubscriber(Node):
+class OdomSubscriber(Node):
     def __init__(self):
-        super().__init__('odometry_sub')
-
-        self.create_subscription(RoverOdometry, 'rover_odom',
-                                 self.odom_cb, 10)
-
+        super().__init__('odom_subscriber')
+        self.subscription = self.create_subscription(
+            RoverOdometry, '/odom_data', self.listener_callback, 10)
+        
+        # Current Position
         self.x = 0.0
         self.y = 0.0
 
-    def odom_cb(self, msg):
+    def listener_callback(self, msg):
         v = msg.linear_velocity.linear.x
-        w = msg.angular_velocity
         theta = msg.orientation
-
-        dt = 1.0
+        dt = 1.0 # Since publisher sends every 1 second
+        
+        # Update coordinates
         self.x += v * math.cos(theta) * dt
         self.y += v * math.sin(theta) * dt
-
-        print(f"\n--- Rover Odometry ---")
-        print(f"ID: {msg.rover_id}")
-        print(f"Orientation: {theta:.2f} rad")
-        print(f"Position: ({self.x:.2f}, {self.y:.2f})")
-        print(f"Linear velocity: {v:.2f} m/s")
-        print(f"Angular velocity: {w:.2f} rad/s")
-
+        
+        # Warning check
         if v > 3.0:
-            print("WARNING: Linear velocity exceeds safe limit!")
+            self.get_logger().warn(f"SPEED WARNING: {v:.2f} m/s exceeds limit!")
+            
+        print(f"Position: x={self.x:.2f}, y={self.y:.2f} | Orientation: {theta:.2f}")
 
 def main(args=None):
     rclpy.init(args=args)
-    node = OdometrySubscriber()
+    node = OdomSubscriber()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
